5ea59994be74664650c98968df1709d29878641c
inaayy <nurussakinah2205@gmail.com>
2026-02-03 10:51:38 +0700

    commit session-6

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes"
+import { authenticate } from "./middlewares/auth.middelware";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Sporton Backend API is Running");
+});
+
+app.get("/test-middleware", authenticate, (req, res) => {
+    res.send("Endpoint ini sekarang tidak bisa diakses public!");
+});
+
+
+export default app;
