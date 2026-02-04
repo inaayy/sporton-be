090aa06a3dd965c2d8abbcf4b2ed2dd4c5dd632c
inaayy <nurussakinah2205@gmail.com>
2026-02-04 20:31:56 +0700

    commit session-7

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.routes"
+import authRoutes from "./routes/auth.routes";
+import categoryRoutes from "./routes/category.routes";
+import productRoutes from "./routes/product.routes";
 import { authenticate } from "./middlewares/auth.middelware";
-
+import path from "path";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Sporton Backend API is Running");
+  res.send("Sporton Backend API is Running");
 });
 
 app.get("/test-middleware", authenticate, (req, res) => {
-    res.send("Endpoint ini sekarang tidak bisa diakses public!");
+  res.send("Endpoint ini sekarang tidak bisa diakses public!");
 });
-
 
 export default app;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signin, initiateAdmin } from "../controllers/auth.controller";
+import { upload } from "../middlewares/upload.middleware";
 
 const router = Router();
 
-router.post("/signin", signin);
+router.post("/signin", upload.none(), signin);
 router.post("/initiate-admin-user", initiateAdmin);
 
 export default router;
